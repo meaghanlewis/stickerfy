f4c7e43325998af7ccedbf60342fbe208bee9eec
Meaghan Lewis <meaghanlewis@github.com>
2018-07-22 13:11:54 -0700

    update mocha tests

--- test/integration/order.spec.js
+++ test/integration/order.spec.js
@@ -1,5 +1,5 @@
-const Cart = require('../models/cart');
-const Product = require('../models/product');
+const Cart = require('../../models/cart');
+const Product = require('../../models/product');
 
 let cart;
 let product;
